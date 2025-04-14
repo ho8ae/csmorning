36d7e2c0063182a6b8048f736e33c025bf66291f
ho8ae <xogh2242@gmail.com>
2025-04-14 22:41:12 +0900

    refactor: Nav components change

--- front/src/pages/LandingPage.jsx
+++ front/src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import Navbar from '../components/common/Navbar';
+import Navbar from '../components/landing/Navbar';
 import Hero from '../components/landing/Hero';
 import Features from '../components/landing/Features';
 import HowItWorks from '../components/landing/HowItWorks';
