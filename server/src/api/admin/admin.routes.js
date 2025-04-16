4dd9f92bb9bebdb7490d30d0dfbfbf94ff3025af
ho8ae <xogh2242@gmail.com>
2025-04-16 20:39:35 +0900

    fix : refreshToken 인증 권한 설정

--- server/src/api/admin/admin.routes.js
+++ server/src/api/admin/admin.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require('./admin.controller');
 const validate = require('../../middleware/validation.middleware');
 const adminValidation = require('./admin.validation');
-const { authAdmin } = require('../../middleware/auth.middleware');  // JWT 인증 미들웨어 불러오기
+const { authAdmin, isAuthenticated, isAdmin } = require('../../middleware/auth.middleware');  // JWT 인증 미들웨어 불러오기
 
 // 모든 라우트에 JWT 인증 미들웨어 적용
 router.use(authAdmin);  // isAuthenticated와 isAdmin을 함께 체크하는 미들웨어
@@ -12,7 +12,7 @@ router.use(authAdmin);  // isAuthenticated와 isAdmin을 함께 체크하는 미
 router.get('/users', adminController.getAllUsers);
 
 //
-router.post('/kakao-tokens', adminController.setKakaoTokens);
+router.post('/kakao-tokens',isAuthenticated,isAdmin, adminController.setKakaoTokens);
 
 // 응답 통계
 router.get('/stats/responses', adminController.getResponseStats);
