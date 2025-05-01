976eb7505810d9bd550fa965441fe101b2a03c91
ho8ae <xogh2242@gmail.com>
2025-05-01 21:57:32 +0900

    test: 주간 모드 테스트

--- server/src/api/webhook/webhook.service.js
+++ server/src/api/webhook/webhook.service.js
@@ -695,7 +695,7 @@ const updateUserStudyMode = async (prisma, userId, studyMode) => {
  * @returns {number} 현재 주차 번호
  */
 const getCurrentWeekNumber = () => {
-  const startDate = new Date(2023, 0, 1);
+  const startDate = new Date(2025, 4, 1);
   const currentDate = new Date();
   const diffTime = Math.abs(currentDate - startDate);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
